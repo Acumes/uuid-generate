0e4b600b79ff84628b74f6db47440fe1424254af
acumes <295277302@qq.com>
2022-03-01 10:26:20 +0800

    dev: 添加日志输出

--- util/utils/uuid-generate.go
+++ util/utils/uuid-generate.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"github.com/go-basic/uuid"
 	"github.com/shirou/gopsutil/host"
 	"os"
@@ -13,10 +12,8 @@ func UuidGenerate(ids []string) string {
 	var file_path = path + "/uuid.txt"
 	//判断文件是否存在，如果存在，读取文件id
 	fileExist, _ := PathExists(file_path)
-	fmt.Println("fileExist==============", fileExist)
 	if fileExist {
 		id, err := ReadFile(file_path)
-		fmt.Println("id========================", id, "======err==============", err)
 		if err == nil {
 			return id
 		}
@@ -25,24 +22,18 @@ func UuidGenerate(ids []string) string {
 	if err != nil {
 		hostNo = uuid.New()
 	}
-	fmt.Println("==========ids==========", ids)
 	if len(ids) > 0 {
 		//判断是否存在ids里面
 		for _, val := range ids {
 			val = strings.Replace(val, " ", "", -1)
 			val = strings.Replace(val, "\n", "", -1)
-			fmt.Println("==========val==========", val, "========", val == hostNo, "=========hostNo=========", hostNo)
 			if val == hostNo {
-				fmt.Println("====================", true)
 				hostNo = uuid.New()
 			}
 		}
 	}
 	//写入文件
-	err = WriteFile(file_path, []byte(hostNo), 0666)
-	if err != nil {
-		fmt.Println("errrr==============", err)
-	}
+	WriteFile(file_path, []byte(hostNo), 0666)
 	return hostNo
 
 }
